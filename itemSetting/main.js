5dfc50240c200a854996f3cbdfc9ddaa3e1c9e65
Onebellfortune <37947858+Onebellfortune@users.noreply.github.com>
2022-02-19 19:21:45 +0900

    API Loading Test
    2022-Feb-19 19:21 PM
    API Mugi loading test

--- itemSetting/main.js
+++ itemSetting/main.js
@@ -1,42 +1,48 @@
+var defaultURL = "https://maplestory.io/api";
+var region = "KMS";
+var version = "360";
+
 function callAPI(url) {
-    return $.ajax({
-        url,
-        type: "GET",
-        success: function (res) {
-            getAllWeapons(res);
-            
-            console.log('뭐래');
-        },
-        error: function () {},
-        complete: function () {},
-    });
+	return $.ajax({
+		url,
+		type: "GET",
+		success: function (res) {
+			getAllWeapons(res);
+		},
+		error: function () {},
+		complete: function () {},
+	});
 }
 
-function getAllWeapons(elements)
-{
-    let weapons = [];
-    elements.forEach(element => {
-        if (
-                (element.typeInfo.category === "Two-Handed Weapon" ||
-                element.typeInfo.category === "One-Handed Weapon")
-                && element.requiredLevel=== 200
-            )
-            
-                weapons.push(element);
-    });
-    console.log(weapons);
-
+let weapons = [];
+function getAllWeapons(elements) {
+	elements.forEach((element) => {
+		if (
+			(element.typeInfo.category === "Two-Handed Weapon" ||
+				element.typeInfo.category === "One-Handed Weapon") &&
+			element.requiredLevel === 200
+		)
+			weapons.push(element);
+	});
+	//console.log(weapons);
 }
-var column=document.querySelectorAll('.item-slot1');
+
+var column = document.querySelectorAll(".item-slot1");
 //console.log(column);
-var weapon=document.getElementById("weapon");
+var weapon = document.getElementById("weapon");
 
-var weaponImg=document.createElement('img');
+callAPI("https://maplestory.io/api/KMS/360/item/category/Equip").then(
+	(result) => {
+		for (var i = 0; i < weapons.length; i++) {
+			var weaponImg = document.createElement("img");
+			imgURI = `${defaultURL}/${region}/${version}/item/${weapons[i].id}/iconRaw`;
+			weaponImg.src = imgURI;
+			document.body.appendChild(weaponImg);
+		}
+	}
+);
 
-var testAPI=callAPI('https://maplestory.io/api/KMS/360/item/category/Equip');
-weaponImg.src='https://maplestory.io/api/KMS/360/item/1213022/icon';
-
+//console.log(weapons);
 weapon.appendChild(weaponImg);
 //weapon.src=callAPI('https://maplestory.io/api/KMS/360/item/1213022/icon');
 //console.log(weapon.src);
-
