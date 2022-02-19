5d6d727624d5c4897a43b08b093e6cc86bc988fe
Onebellfortune <37947858+Onebellfortune@users.noreply.github.com>
2022-02-19 18:37:30 +0900

    Genesis weapopn test
    2022-Feb-19
    Maplestory API
    Genesis weapon filtering test

--- itemSetting/main.js
+++ itemSetting/main.js
@@ -1,5 +1,42 @@
+function callAPI(url) {
+    return $.ajax({
+        url,
+        type: "GET",
+        success: function (res) {
+            getAllWeapons(res);
+            
+            console.log('뭐래');
+        },
+        error: function () {},
+        complete: function () {},
+    });
+}
+
+function getAllWeapons(elements)
+{
+    let weapons = [];
+    elements.forEach(element => {
+        if (
+                (element.typeInfo.category === "Two-Handed Weapon" ||
+                element.typeInfo.category === "One-Handed Weapon")
+                && element.requiredLevel=== 200
+            )
+            
+                weapons.push(element);
+    });
+    console.log(weapons);
+
+}
 var column=document.querySelectorAll('.item-slot1');
-console.log(column);
+//console.log(column);
 var weapon=document.getElementById("weapon");
-console.log(weapon);
+
+var weaponImg=document.createElement('img');
+
+var testAPI=callAPI('https://maplestory.io/api/KMS/360/item/category/Equip');
+weaponImg.src='https://maplestory.io/api/KMS/360/item/1213022/icon';
+
+weapon.appendChild(weaponImg);
+//weapon.src=callAPI('https://maplestory.io/api/KMS/360/item/1213022/icon');
+//console.log(weapon.src);
 
