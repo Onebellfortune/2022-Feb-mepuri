6768a906f931ef986fd6eac4ecfb65e3b72273d4
evolutionHan <wowzzin@gmail.com>
2022-03-23 18:05:27 +0900

    uglify 추가
    webpack uglify 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
+    // mode: "development",
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+    },
     entry: {
         mainCodi: "./codi/main.js",
-        mainSeed: "./seedRing/main.js"
+        mainSeed: "./seedRing/main.js",
     },
     output: {
         filename: "[name].main.js",
